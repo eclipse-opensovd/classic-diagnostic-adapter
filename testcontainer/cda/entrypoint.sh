f0b46e1b20c09e7427a0dde6b4808dab5dafe2b8
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2025-11-19 16:16:28 +0100

    chore: use the same copyright header everywhere and cover missing files
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- testcontainer/cda/entrypoint.sh
+++ testcontainer/cda/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh -e
+# Copyright (c) 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+
 
 DOIP_TESTER_IP=$(ip -4 a show scope global | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
