bd2b3df8e1d7920dcfb6fa508e1e3947584d518d
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2025-12-06 18:01:02 +0100

    testing: mock web server types and add simple test
    Adjust traits, so mocking is possible.
    Create a test, which verifies if receiving DTCs
    is working with the mocked data types
    
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- cda-comm-uds/src/lib.rs
+++ cda-comm-uds/src/lib.rs
@@ -17,6 +17,7 @@ use std::{
     time::{Duration, Instant},
 };
 
+use async_trait::async_trait;
 use cda_interfaces::{
     DiagComm, DiagCommType, DiagServiceError, DynamicPlugin, EcuGateway, EcuManager, EcuVariant,
     FlashTransferStartParams, HashMap, HashMapExtensions, HashSet, HashSetExtensions,
@@ -940,6 +941,7 @@ impl<S: EcuGateway, R: DiagServiceResponse, T: EcuManager<Response = R>> Clone
     }
 }
 
+#[async_trait]
 impl<S: EcuGateway, R: DiagServiceResponse, T: EcuManager<Response = R>> UdsEcu
     for UdsManager<S, R, T>
 {
