740ac8bca75c5bee472f6c30c2cd4d332b5d1412
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2026-02-13 14:41:07 +0100

    chore: update copyright
    Updates the project to the new reuse tool copyright header.
    
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- cda-core/src/diag_kernel/iso_14229_nrc.rs
+++ cda-core/src/diag_kernel/iso_14229_nrc.rs
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -7,8 +8,6 @@
  * This program and the accompanying materials are made available under the
  * terms of the Apache License Version 2.0 which is available at
  * https://www.apache.org/licenses/LICENSE-2.0
- *
- * SPDX-License-Identifier: Apache-2.0
  */
 
 pub const fn get_nrc_code(val: u8) -> &'static str {
