d2292ac108ade33e4cac1c57424469f946244afd
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2026-02-13 14:41:07 +0100

    chore: update copyright
    Updates the project to the new reuse tool copyright header.
    
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- gen_protos.sh
+++ gen_protos.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-# Copyright (c) 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
+
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
 #
 # See the NOTICE file(s) distributed with this work for additional
 # information regarding copyright ownership.
@@ -7,8 +9,6 @@
 # This program and the accompanying materials are made available under the
 # terms of the Apache License Version 2.0 which is available at
 # https://www.apache.org/licenses/LICENSE-2.0
-#
-# SPDX-License-Identifier: Apache-2.0
 
 ## Call cargo check to trigger the build script with the feature `gen-protos` enabled.
 cargo check -p cda-database --features gen-protos
--- testcontainer/odx/generate.py
+++ testcontainer/odx/generate.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
 #
 # See the NOTICE file(s) distributed with this work for additional
 # information regarding copyright ownership.
@@ -6,8 +7,6 @@
 # This program and the accompanying materials are made available under the
 # terms of the Apache License Version 2.0 which is available at
 # https://www.apache.org/licenses/LICENSE-2.0
-#
-# SPDX-License-Identifier: Apache-2.0
 
 import odxtools
 from odxtools.database import Database
