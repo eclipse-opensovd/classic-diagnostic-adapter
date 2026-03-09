799a1be4c96ecc29bafe9a8b046faf53d4aa7b61
Elena Gantner <elena.gantner@mercedes-benz.com>
2026-03-09 10:11:58 +0100

    TLS: add mbedtls variant
     as openssl does not support the record size limit extension
     this commit introduces the feature to build with mbedtls instead.
    
    Signed-off-by: Elena Gantner <elena.gantner@mercedes-benz.com>

--- cda-comm-doip/src/ecu_connection.rs
+++ cda-comm-doip/src/ecu_connection.rs
@@ -10,41 +10,101 @@
  * https://www.apache.org/licenses/LICENSE-2.0
  */
 
-use std::{pin::Pin, time::Duration};
+use std::time::Duration;
 
 use cda_interfaces::{DiagServiceError, dlt_ctx};
 use doip_definitions::{
     message::DoipMessage,
     payload::{ActivationCode, DoipPayload, RoutingActivationRequest, RoutingActivationResponse},
 };
+#[cfg(all(feature = "mbedtls", not(feature = "openssl")))]
+use mbedtls_rs::{
+    async_stream::TlsStream,
+    ssl::{SslConfigBuilder, SslVerifyMode, TlsVersion},
+};
+#[cfg(feature = "openssl")]
 use openssl::ssl::{Ssl, SslContextBuilder, SslMethod, SslOptions, SslVerifyMode, SslVersion};
 use tokio::{
     net::{TcpSocket, TcpStream},
     sync::Mutex,
 };
-use tokio_openssl::SslStream;
+#[cfg(feature = "openssl")]
+use tokio_openssl::SslStream as TlsStream;
 
 use crate::{
     ConnectionError,
     socket::{DoIPConfig, DoIPConnection, DoIPConnectionReadHalf, DoIPConnectionWriteHalf},
 };
-const ENABLED_SSL_CIPHERS: [&str; 4] = [
-    "ECDHE-RSA-AES128-GCM-SHA256",
-    "ECDHE-ECDSA-AES128-SHA256",
-    "ECDHE-ECDSA-NULL-SHA",
-    "TLS_FALLBACK_SCSV",
-];
 
-const ELIPTIC_CURVE_GROUPS: [&str; 8] = [
-    "x25519",
-    "secp256r1",
-    "secp384r1",
-    "x448",
-    "secp521r1",
-    "brainpoolP256r1",
-    "brainpoolP384r1",
-    "brainpoolP512r1",
-];
+/// This module contains the (currently) static TLS configuration for the CDA
+///
+/// In the future this should be configurable via configurationfile instead.
+mod tlsconfig {
+    #[cfg(all(feature = "mbedtls", not(feature = "openssl")))]
+    use mbedtls_rs::ffi::{
+        MBEDTLS_SSL_EMPTY_RENEGOTIATION_INFO, MBEDTLS_SSL_IANA_TLS_GROUP_BP256R1,
+        MBEDTLS_SSL_IANA_TLS_GROUP_BP384R1, MBEDTLS_SSL_IANA_TLS_GROUP_BP512R1,
+        MBEDTLS_SSL_IANA_TLS_GROUP_SECP256R1, MBEDTLS_SSL_IANA_TLS_GROUP_SECP384R1,
+        MBEDTLS_SSL_IANA_TLS_GROUP_SECP521R1, MBEDTLS_SSL_IANA_TLS_GROUP_X448,
+        MBEDTLS_SSL_IANA_TLS_GROUP_X25519, MBEDTLS_TLS_ECDHE_ECDSA_WITH_NULL_SHA,
+        MBEDTLS_TLS1_3_SIG_ECDSA_SECP256R1_SHA256, MBEDTLS_TLS1_3_SIG_ECDSA_SECP384R1_SHA384,
+        MBEDTLS_TLS1_3_SIG_ECDSA_SECP521R1_SHA512, MBEDTLS_TLS1_3_SIG_ECDSA_SHA1,
+        MBEDTLS_TLS1_3_SIG_ED25519, MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA1,
+        MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA256, MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA384,
+        MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA512,
+    };
+
+    #[cfg(feature = "openssl")]
+    pub const ENABLED_SSL_CIPHERS: [&str; 4] = [
+        "ECDHE-RSA-AES128-GCM-SHA256",
+        "ECDHE-ECDSA-AES128-SHA256",
+        "ECDHE-ECDSA-NULL-SHA",
+        "TLS_FALLBACK_SCSV",
+    ];
+
+    #[cfg(all(feature = "mbedtls", not(feature = "openssl")))]
+    pub const MBEDTLS_SSL_CIPHERSUITES: [i32; 2] = [
+        MBEDTLS_TLS_ECDHE_ECDSA_WITH_NULL_SHA as i32,
+        MBEDTLS_SSL_EMPTY_RENEGOTIATION_INFO as i32,
+    ];
+
+    #[cfg(feature = "openssl")]
+    pub const ELIPTIC_CURVE_GROUPS: [&str; 8] = [
+        "x25519",
+        "secp256r1",
+        "secp384r1",
+        "x448",
+        "secp521r1",
+        "brainpoolP256r1",
+        "brainpoolP384r1",
+        "brainpoolP512r1",
+    ];
+
+    #[cfg(all(feature = "mbedtls", not(feature = "openssl")))]
+    pub const MBEDTLS_CURVE_GROUPS: [u16; 8] = [
+        MBEDTLS_SSL_IANA_TLS_GROUP_X25519 as u16,
+        MBEDTLS_SSL_IANA_TLS_GROUP_SECP256R1 as u16,
+        MBEDTLS_SSL_IANA_TLS_GROUP_SECP384R1 as u16,
+        MBEDTLS_SSL_IANA_TLS_GROUP_X448 as u16,
+        MBEDTLS_SSL_IANA_TLS_GROUP_SECP521R1 as u16,
+        MBEDTLS_SSL_IANA_TLS_GROUP_BP256R1 as u16,
+        MBEDTLS_SSL_IANA_TLS_GROUP_BP384R1 as u16,
+        MBEDTLS_SSL_IANA_TLS_GROUP_BP512R1 as u16,
+    ];
+
+    #[cfg(all(feature = "mbedtls", not(feature = "openssl")))]
+    pub const MBEDTLS_SIGNATURE_ALGORITHMS: [u16; 9] = [
+        MBEDTLS_TLS1_3_SIG_ECDSA_SECP521R1_SHA512 as u16,
+        MBEDTLS_TLS1_3_SIG_ECDSA_SECP384R1_SHA384 as u16,
+        MBEDTLS_TLS1_3_SIG_ECDSA_SECP256R1_SHA256 as u16,
+        MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA512 as u16,
+        MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA384 as u16,
+        MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA256 as u16,
+        MBEDTLS_TLS1_3_SIG_ECDSA_SHA1 as u16,
+        MBEDTLS_TLS1_3_SIG_RSA_PKCS1_SHA1 as u16,
+        MBEDTLS_TLS1_3_SIG_ED25519 as u16,
+    ];
+}
 
 #[derive(Clone)]
 pub(crate) struct ConnectionConfig {
@@ -66,13 +126,15 @@ pub(crate) trait ECUConnectionSend {
 }
 
 enum EcuConnectionVariant {
-    Tls(DoIPConnection<tokio_openssl::SslStream<TcpStream>>),
+    #[cfg(any(feature = "openssl", feature = "mbedtls"))]
+    Tls(DoIPConnection<TlsStream<TcpStream>>),
     Plain(DoIPConnection<TcpStream>),
 }
 
 impl EcuConnectionVariant {
     async fn send(&mut self, msg: DoipPayload) -> Result<(), ConnectionError> {
         match self {
+            #[cfg(any(feature = "openssl", feature = "mbedtls"))]
             EcuConnectionVariant::Tls(conn) => conn.send(msg).await,
             EcuConnectionVariant::Plain(conn) => conn.send(msg).await,
         }
@@ -80,12 +142,14 @@ impl EcuConnectionVariant {
     }
     async fn read(&mut self) -> Option<Result<DoipMessage, ConnectionError>> {
         match self {
+            #[cfg(any(feature = "openssl", feature = "mbedtls"))]
             EcuConnectionVariant::Tls(conn) => conn.read().await,
             EcuConnectionVariant::Plain(conn) => conn.read().await,
         }
     }
     fn into_split(self) -> (EcuConnectionReadVariant, EcuConnectionSendVariant) {
         match self {
+            #[cfg(any(feature = "openssl", feature = "mbedtls"))]
             EcuConnectionVariant::Tls(conn) => {
                 let (read, write) = conn.into_split();
                 (
@@ -105,12 +169,14 @@ impl EcuConnectionVariant {
 }
 
 pub(crate) enum EcuConnectionReadVariant {
-    Tls(DoIPConnectionReadHalf<tokio_openssl::SslStream<tokio::net::TcpStream>>),
-    Plain(DoIPConnectionReadHalf<tokio::net::TcpStream>),
+    #[cfg(any(feature = "openssl", feature = "mbedtls"))]
+    Tls(DoIPConnectionReadHalf<TlsStream<TcpStream>>),
+    Plain(DoIPConnectionReadHalf<TcpStream>),
 }
 pub(crate) enum EcuConnectionSendVariant {
-    Tls(DoIPConnectionWriteHalf<tokio_openssl::SslStream<tokio::net::TcpStream>>),
-    Plain(DoIPConnectionWriteHalf<tokio::net::TcpStream>),
+    #[cfg(any(feature = "openssl", feature = "mbedtls"))]
+    Tls(DoIPConnectionWriteHalf<TlsStream<TcpStream>>),
+    Plain(DoIPConnectionWriteHalf<TcpStream>),
 }
 
 pub(crate) struct EcuConnectionTarget {
@@ -184,6 +250,7 @@ impl EcuConnectionTarget {
 impl ECUConnectionSend for EcuConnectionSendVariant {
     async fn send(&mut self, msg: DoipPayload) -> Result<(), ConnectionError> {
         match self {
+            #[cfg(any(feature = "openssl", feature = "mbedtls"))]
             EcuConnectionSendVariant::Tls(conn) => conn.send(msg).await,
             EcuConnectionSendVariant::Plain(conn) => conn.send(msg).await,
         }
@@ -194,6 +261,7 @@ impl ECUConnectionSend for EcuConnectionSendVariant {
 impl ECUConnectionRead for EcuConnectionReadVariant {
     async fn read(&mut self) -> Option<Result<DoipMessage, ConnectionError>> {
         match self {
+            #[cfg(any(feature = "openssl", feature = "mbedtls"))]
             EcuConnectionReadVariant::Tls(conn) => conn.read().await,
             EcuConnectionReadVariant::Plain(conn) => conn.read().await,
         }
@@ -364,65 +432,7 @@ pub(crate) async fn establish_tls_ecu_connection(
     )
     .await
     {
-        Ok(Ok(stream)) => {
-            // allow unsafe ciphers
-            let mut builder = SslContextBuilder::new(SslMethod::tls_client()).map_err(|e| {
-                ConnectionError::ConnectionFailed(format!(
-                    "Failed to create SSL context builder: {e:?}"
-                ))
-            })?;
-
-            builder
-                .set_cipher_list(&ENABLED_SSL_CIPHERS.join(":"))
-                .map_err(|e| {
-                    ConnectionError::ConnectionFailed(format!("Failed to set cipher list: {e:?}"))
-                })?;
-            builder.set_verify(SslVerifyMode::NONE);
-            // necessary for NULL encryption
-            builder.set_security_level(0);
-            builder
-                .set_min_proto_version(Some(SslVersion::TLS1_2))
-                .map_err(|e| {
-                    ConnectionError::ConnectionFailed(format!(
-                        "Failed to set minimum TLS version: {e:?}"
-                    ))
-                })?;
-            builder
-                .set_max_proto_version(Some(SslVersion::TLS1_3))
-                .map_err(|e| {
-                    ConnectionError::ConnectionFailed(format!(
-                        "Failed to set maximum TLS version: {e:?}"
-                    ))
-                })?;
-
-            builder
-                .set_groups_list(&ELIPTIC_CURVE_GROUPS.join(":"))
-                .map_err(|e| {
-                    ConnectionError::ConnectionFailed(format!(
-                        "Failed to set elliptic curve groups: {e:?}"
-                    ))
-                })?;
-
-            let preset_options = builder.options();
-            // this is the flag legacy_renegotiation in openssl client
-            builder
-                .set_options(preset_options.union(SslOptions::ALLOW_UNSAFE_LEGACY_RENEGOTIATION));
-
-            let ctx = builder.build();
-            let ssl = Ssl::new(&ctx).map_err(|e| {
-                ConnectionError::ConnectionFailed(format!("Failed to create SSL context: {e:?}"))
-            })?;
-
-            let mut stream = SslStream::new(ssl, stream).map_err(|e| {
-                ConnectionError::ConnectionFailed(format!("Failed to create SSL stream: {e:?}"))
-            })?;
-            // wait for the actual connection .
-            Pin::new(&mut stream).connect().await.map_err(|e| {
-                ConnectionError::ConnectionFailed(format!("Unable to Pin SSL connection: {e}"))
-            })?;
-
-            EcuConnectionVariant::Tls(DoIPConnection::new(stream, doip_connection_config))
-        }
+        Ok(Ok(stream)) => create_tls_stream(stream, doip_connection_config).await?,
         Ok(Err(e)) => {
             return Err(ConnectionError::ConnectionFailed(format!(
                 "Connect failed: {e:?}"
@@ -474,6 +484,106 @@ pub(crate) async fn establish_tls_ecu_connection(
             "Failed to activate routing: {e:?}"
         ))),
     }
+}
+
+#[cfg(all(feature = "mbedtls", not(feature = "openssl")))]
+async fn create_tls_stream(
+    stream: tokio::net::TcpStream,
+    doip_connection_config: DoIPConfig,
+) -> Result<EcuConnectionVariant, ConnectionError> {
+    let config = SslConfigBuilder::new_client()
+        .map_err(|e| {
+            ConnectionError::ConnectionFailed(format!("Failed to create SSL config: {e:?}"))
+        })?
+        .max_tls_version(TlsVersion::Tls12)
+        .min_tls_version(TlsVersion::Tls12)
+        .verify_mode(SslVerifyMode::None)
+        .ciphersuites(tlsconfig::MBEDTLS_SSL_CIPHERSUITES.as_slice())
+        .groups(tlsconfig::MBEDTLS_CURVE_GROUPS.as_slice())
+        .sig_algs(tlsconfig::MBEDTLS_SIGNATURE_ALGORITHMS.as_slice())
+        .build();
+
+    let ssl = TlsStream::connect(config, stream, None)
+        .await
+        .map_err(|e| {
+            ConnectionError::ConnectionFailed(format!("Failed to create SSL stream: {e:?}"))
+        })?;
+    Ok(EcuConnectionVariant::Tls(DoIPConnection::new(
+        ssl,
+        doip_connection_config,
+    )))
+}
+
+#[cfg(feature = "openssl")]
+async fn create_tls_stream(
+    stream: tokio::net::TcpStream,
+    doip_connection_config: DoIPConfig,
+) -> Result<EcuConnectionVariant, ConnectionError> {
+    // allow unsafe ciphers
+    let mut builder = SslContextBuilder::new(SslMethod::tls_client()).map_err(|e| {
+        ConnectionError::ConnectionFailed(format!("Failed to create SSL context builder: {e:?}"))
+    })?;
+
+    builder
+        .set_cipher_list(&tlsconfig::ENABLED_SSL_CIPHERS.join(":"))
+        .map_err(|e| {
+            ConnectionError::ConnectionFailed(format!("Failed to set cipher list: {e:?}"))
+        })?;
+    builder.set_verify(SslVerifyMode::NONE);
+    // necessary for NULL encryption
+    builder.set_security_level(0);
+    builder
+        .set_min_proto_version(Some(SslVersion::TLS1_2))
+        .map_err(|e| {
+            ConnectionError::ConnectionFailed(format!("Failed to set minimum TLS version: {e:?}"))
+        })?;
+    builder
+        .set_max_proto_version(Some(SslVersion::TLS1_3))
+        .map_err(|e| {
+            ConnectionError::ConnectionFailed(format!("Failed to set maximum TLS version: {e:?}"))
+        })?;
+
+    builder
+        .set_groups_list(&tlsconfig::ELIPTIC_CURVE_GROUPS.join(":"))
+        .map_err(|e| {
+            ConnectionError::ConnectionFailed(format!("Failed to set elliptic curve groups: {e:?}"))
+        })?;
+
+    let preset_options = builder.options();
+    // this is the flag legacy_renegotiation in openssl client
+    builder.set_options(preset_options.union(SslOptions::ALLOW_UNSAFE_LEGACY_RENEGOTIATION));
+
+    let ctx = builder.build();
+    let ssl = Ssl::new(&ctx).map_err(|e| {
+        ConnectionError::ConnectionFailed(format!("Failed to create SSL context: {e:?}"))
+    })?;
+
+    let mut stream = TlsStream::new(ssl, stream).map_err(|e| {
+        ConnectionError::ConnectionFailed(format!("Failed to create SSL stream: {e:?}"))
+    })?;
+    // wait for the actual connection .
+    std::pin::Pin::new(&mut stream)
+        .connect()
+        .await
+        .map_err(|e| {
+            ConnectionError::ConnectionFailed(format!("Unable to Pin SSL connection: {e}"))
+        })?;
+
+    Ok(EcuConnectionVariant::Tls(DoIPConnection::new(
+        stream,
+        doip_connection_config,
+    )))
+}
+
+// Allow building CDA without TLS support
+#[cfg(all(not(feature = "openssl"), not(feature = "mbedtls")))]
+async fn create_tls_stream(
+    _stream: tokio::net::TcpStream,
+    _doip_connection_config: DoIPConfig,
+) -> Result<EcuConnectionVariant, ConnectionError> {
+    Err(ConnectionError::ConnectionFailed(
+        "CDA built without TLS support.".to_owned(),
+    ))
 }
 
 // Allow the underscore bindings because the variables
