b70e867f723cb980106a56c8d40d0cd78c32a36e
Elena Gantner <elena.gantner@mercedes-benz.com>
2026-06-08 11:37:22 +0200

    chore: fix clippy mut refs warning in ecumanager
    Signed-off-by: Elena Gantner <elena.gantner@mercedes-benz.com>

--- cda-core/src/diag_kernel/ecumanager.rs
+++ cda-core/src/diag_kernel/ecumanager.rs
@@ -165,7 +165,7 @@ struct DbCache {
 }
 
 impl DbCache {
-    pub(crate) async fn reset(&mut self) {
+    pub(crate) async fn reset(&self) {
         self.diag_services.write().await.clear();
     }
 }
@@ -4290,7 +4290,7 @@ impl<S: SecurityPlugin> EcuManager<S> {
         &self,
         mapped_service: &datatypes::DiagService,
         uds_payload: &mut Payload,
-        data: &mut MappedDiagServiceResponsePayload,
+        data: &MappedDiagServiceResponsePayload,
         repeated_dop: &datatypes::DopField,
         start: usize,
     ) -> Result<(HashMap<String, DiagDataTypeContainer>, usize), DiagServiceError> {
