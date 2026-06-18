7995ca6b252e3fb1887a1ceb2f0f628f822f2d2d
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2026-06-18 10:09:22 +0200

    chore: update copyright headers
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- cda-comm-doip/src/config.rs
+++ cda-comm-doip/src/config.rs
@@ -1,6 +1,5 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
- * SPDX-FileCopyrightText: 2025 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
+ * SPDX-FileCopyrightText: 2025 Copyright (c) Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -8,6 +7,8 @@
  * This program and the accompanying materials are made available under the
  * terms of the Apache License Version 2.0 which is available at
  * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 use cda_interfaces::Protocol;
